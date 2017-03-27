a61547478b4a4e55bda8c5cbdeef4fd9bb7c9bd4
vdib <adib.ali@outlook.com>
2017-03-27 23:42:16 +0200

    Added personal info
    Declared an object with key value pairs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,33 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Adib Ali");
+var name = "Adib Ali";
+var role = "Student";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName + formattedRole);
+
+var skills = ["Cooking", "Sleeping", "Procrastinating"];
+
+var bio = {
+	"name" : name,
+	"role" : role,
+	"mobile" : "017631496009",
+	"email" : "md.adib.ali@gmail.com",
+	"twitter" : "twitter.com/adib__ali",
+	"picture_URL" : "images/fry.jpg",
+	"welcome_message" : "Konnichiwa bitches",
+	"skills" : skills
+
+}
+
+var mobile = HTMLmobile.replace("%data%", bio.mobile);
+var email = HTMLemail.replace("%data%", bio.email);
+var twitter = HTMLtwitter.replace("%data%", bio.twitter);
+var bioPic = HTMLbioPic.replace("%data%", bio.picture_URL);
+var welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
+var skills = HTMLskills.replace("%data%", bio.skills);
+
+$("#header").append(mobile + email + twitter + bioPic + welcomeMsg + skills);
