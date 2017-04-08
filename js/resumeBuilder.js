6abca03e92fd341413b613f9d9481d7b158c40e2
vdib <adib.ali@outlook.com>
2017-04-08 22:23:55 +0200

    Problem set 1
    Code cleaned and left with 4 objects.
    Project images added
    Element properties changed to black instead of none

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,55 +1,82 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
-*/
 
-var name = "Adib Ali";
-var role = "Student";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+// $("#header").prepend(formattedName + formattedRole);
 
-$("#header").prepend(formattedName + formattedRole);
+var work = {
+	"jobs" : [{
+		"employer" : "Lufthansa Technik",
+		"title" : "Werkstudent",
+		"location" : "Hamburg",
+		"dates" : "Sept - Dec 2016",
+		"description" : "Inventory with SAP"
+	}]
+};
 
-var skills = ["Cooking", "Sleeping", "Procrastinating"];
-
-var bio = {
-	"name" : name,
-	"role" : role,
-	"mobile" : "017631496009",
-	"email" : "md.adib.ali@gmail.com",
-	"twitter" : "twitter.com/adib__ali",
-	"picture_URL" : "images/fry.jpg",
-	"welcome_message" : "Konnichiwa bitches",
-	"skills" : skills
-
+var projects = {
+	"projects" : [{
+		"title" : "Closed-loop control of locations of users and autonomics vehicles",
+		"dates" : "Winter Semester 16/17",
+		"description" : "Developing autonomous flying system of a drone",
+		"images" : ["images/Arduino.jpg", "images/IMAG2185.jpg", "images/IMAG2186.jpg", "images/IMAG2188.jpg"]
+	}]
 }
 
-var mobile = HTMLmobile.replace("%data%", bio.mobile);
-var email = HTMLemail.replace("%data%", bio.email);
-var twitter = HTMLtwitter.replace("%data%", bio.twitter);
-var bioPic = HTMLbioPic.replace("%data%", bio.picture_URL);
-var welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
-var skills = HTMLskills.replace("%data%", bio.skills);
-var work = {
-	"position" : "Werkstudent",
-	"employer" : "Lufthansa Technik",
-	"years_worked" : 1,
-	"city" : "Hamburg"
-};
+var bio = {
+	"name" : "Muhammad Adib Ali",
+	"role" : "Student",
+	"welcomeMessage" : "Konnichiwa bitches",
+	"biopic" : "Another speck of dust in the vast universe",
+	"contacts" : {
+		"mobile" : "017631496009",
+		"email" : "md.adib.ali@gmail.com",
+		"twitter" : "twitter.com/adib__ali",
+		"github" : "vdib",
+		"location" : "Hamburg"
+	},
+	"skills" : ["Cooking", "Sleeping", "Procrastinating"]
+}
 
 var education = {
 	"schools" : [{
 		"name" : "HAW Hamburg",
-		"years" : "2013-now",
-		"city" : "Hamburg"	
+		"location" : "Hamburg",
+		"degree dates" : "2018",
+		"url" : "haw-hamburg.de",
+		"majors" : ["Electronics", "Software Programming", "Electrical Engineering", "Signal Processing"]	
 	},
 	{
 		"name" : "GMI",
 		"years" : "2011-2013",
-		"city" : "Kajang, Selangor"
-	}]
+		"city" : "Kajang, Selangor",
+		"url" : "http://www.gmi.edu.my/",
+		"majors" : ["GCSE A-Level", "German language"]
+	}],
+	"onlineCourses" : [{
+		"title" : "Programming in Java",
+		"school" : "University of Helsinki",
+		"dates" : "WS 15/16",
+		"url" : "http://mooc.fi/courses/2013/programming-part-1/"
+	},
+	{
+		"title" : "JavaScript basics",
+		"school" : "Udacity",
+		"dates" : "SS 17",
+		"url" : "https://www.udacity.com/course/javascript-basics--ud804"
+	}
+	]
 
 };
 
-$("#header").append(mobile + email + twitter + bioPic + welcomeMsg + skills);
-$("#header").append(work["position"] + '\xa0' + education.schools[0].name);
+// var mobile = HTMLmobile.replace("%data%", bio.mobile);
+// var email = HTMLemail.replace("%data%", bio.email);
+// var twitter = HTMLtwitter.replace("%data%", bio.twitter);
+// var bioPic = HTMLbioPic.replace("%data%", bio.picture_URL);
+// var welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
+// var skills = HTMLskills.replace("%data%", bio.skills);
+
+
+
+// $("#header").append(mobile + email + twitter + bioPic + welcomeMsg + skills);
+// $("#header").append(work["position"] + '\xa0' + education.schools[0].name);
